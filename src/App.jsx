6dcb0ca7021f6cab8369115ad4031cd3fa2970ec
AbdurRob <abdurrobr34l@gmail.com>
2025-09-21 16:04:44 +0600

    Started practicing module-39

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl font-bold">Start Your Coding Here</h1>
+      <p className="my-4">Hello world</p>
+      <button>Button</button>
+    </>
+  );
+}
+
+export default App;
